2206b1d949ff2e1720763ac0728e14a8a3a43b9d
Mohamed Mishal M <mohamedmishal077@gmail.com>
2025-08-23 15:53:30 +0530

    backend url updqte

--- linkhive-backend/db.js
+++ linkhive-backend/db.js
@@ -1,23 +1,9 @@
 import pkg from "pg";
-import dotenv from "dotenv";
+const { Pool } = pkg;
 
-
-const isProd = process.env.NODE_ENV === "production";
-
-const pool = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: isProd ? { rejectUnauthorized: false } : false,
-      }
-    : {
-        user: process.env.DB_USER || "postgres",
-        host: process.env.DB_HOST || "localhost",
-        database: process.env.DB_NAME || "LinkHive",
-        password: process.env.DB_PASSWORD || "",
-        port: Number(process.env.DB_PORT || 5432),
-      }
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
-
